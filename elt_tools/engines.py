2f9a9ab6ba996f5cb57aacd5d021470482a51ffe
Dewald Abrie <dewald@rentalcover.com>
2020-07-13 23:25:37 +1000

    Support extraneous kwargs in engine constructors

--- elt_tools/engines.py
+++ elt_tools/engines.py
@@ -9,24 +9,24 @@ REDSHIFT_ENGINE = 'redshift_engine'
 MOCK_ENGINE = 'mock_engine'
 
 
-def mock_engine():
+def mock_engine(*args, **kwargs):
     return MagicMock()
 
 
-def bigquery_engine(gcp_project=None, dataset_id=None, gcp_credentials=None):
+def bigquery_engine(gcp_project=None, dataset_id=None, gcp_credentials=None, **kwargs):
     bigquery_uri = f'bigquery://{gcp_project}/{dataset_id}'
     engine = create_engine(bigquery_uri, credentials_path=gcp_credentials)
     return engine
 
 
-def redshift_engine(sql_alchemy_conn_string=None, default_schema='public', connect_timeout=3600):
+def redshift_engine(sql_alchemy_conn_string=None, default_schema='public', connect_timeout=3600, **kwargs):
     engine = create_engine(sql_alchemy_conn_string, connect_args={'connect_timeout': connect_timeout})
     engine.execute(text('SET search_path TO %s,public;' % default_schema).execution_options(
         autocommit=True))
     return engine
 
 
-def oltp_engine(sql_alchemy_conn_string=None, connect_timeout=3600):
+def oltp_engine(sql_alchemy_conn_string=None, connect_timeout=3600, **kwargs):
     engine = create_engine(sql_alchemy_conn_string, connect_args={'connect_timeout': connect_timeout})
     return engine
 
